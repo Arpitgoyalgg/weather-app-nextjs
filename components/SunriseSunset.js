7fb879871ecf9bc39d8cacf74b5c822e7c28b03d
arpitgoyalgg <arpitgoyalgg@gmail.com>
2021-04-09 01:14:05 +0530

    fixed SunriseSunset component again

--- components/SunriseSunset.js
+++ components/SunriseSunset.js
@@ -3,8 +3,8 @@ export default function SunriseSunset(props) {
     <>
       <div className="big-card">
         <h3>Sunrise and Sunset</h3>
-        <h2> - {props.sunrise}</h2>
-        <h2> - {props.sunset}</h2>
+        <h2>- {props.sunrise}</h2>
+        <h2>- {props.sunset}</h2>
       </div>
     </>
   );
